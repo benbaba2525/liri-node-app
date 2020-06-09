98c8b54110d9666f1341278567745f2fdece0f2a
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-06-09 00:52:12 -0700

    Add getMovie function

--- liri.js
+++ liri.js
@@ -71,11 +71,46 @@ if ( userSearch === ""){
 
 };
 };
+
 // This will output movie information
 function getMovie(){
 
+    // Then run a request with axios to the OMDB API with the movie specified
+axios.get(`https://www.omdbapi.com/?t=${movieName}&y=&plot=short&apikey=trilogy`).then(
+    function(response) {
+      console.log("Title of the movie : " + response.data.Title);
+      console.log("Year the movie came out : " + response.data.Year);
+      console.log("IMDB Rating of the movie : " + response.data.imdbRating);
+      console.log("Rotten Tomatoes Rating of the movie : " + response.data.Ratings[1].Value);
+      console.log("Country where the movie was produced : " + response.data.Country);
+      console.log("Language of the movie : " + response.data.Language);
+      console.log("Plot of the movie : " + response.data.Plot);
+      console.log("Actor in the movie : " + response.data.Actors);
+    })
+    .catch(function(error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log("---------------Data---------------");
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Status---------------");
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an object that comes back with details pertaining to the error that occurred.
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });
+
 };
 
+getMovie();
 
 // Will run sptify-this-song for "I Want It That Way" as follows the text in random.txt
 function doWhatItSays(){
