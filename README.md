7f333ffe31610f0d9bc17bccd4c16963a08ccbb3
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-06-11 20:44:54 -0700

    Add gif to put on readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # liri-node-app
+
+/Users/ben/Desktop/Class-HW/liri-node-app/Screen Recording 2020-06-10 at 11.12.38 PM.mov
--- liri.js
+++ liri.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 
-
 var keys = require("./keys.js");
 var fs = require("fs");
 var axios = require("axios");
@@ -44,11 +43,17 @@ function getBandsEvents(artists){
     // Then run a request with axios to the OMDB API with the movie specified
 axios.get(`https://rest.bandsintown.com/artists/${artists}/events?app_id=codingbootcamp`).then(
     function(response) {
+      console.log("");
+      console.log("===================== Event Information =====================");
+      console.log("");
       console.log("Name of the venue : " + response.data[0].venue.name);
       console.log("Venue location : " + response.data[0].venue.location);
 
       var eventDate = moment(response.data[0].datetime).format('MMMM Do YYYY, h:mm:ss a');
       console.log("Date of the Event  : " + eventDate);
+      console.log("");
+      console.log("===============================================================");
+      console.log("");
     })
     .catch(function(error) {
       // Something happened in setting up the request that triggered an Error
@@ -59,24 +64,33 @@ axios.get(`https://rest.bandsintown.com/artists/${artists}/events?app_id=codingb
 };
 
 
-
-
 // This will show information about the song 
 function getSong(userSearch){
 
 // If no song is provided then program will run default to "The Sign" by Ace of Base
   if (userSearch === undefined) {
     userSearch = `"The Sign" Ace of Base`;
+    console.log("");
+    console.log("-------------------- Check this song out !! -------------------- ");
+    
+    
 };
 
 spotify
   .search({ type: 'track', query: userSearch  })
   .then(function(data) {
       var result = data.tracks.items[0]
+
+    console.log("");
+    console.log("==================== Information of this song ======================");
+    console.log("");
     console.log("Artist(s) : " , result.album.artists[0].name);
     console.log("Song Name : " , result.name);
     console.log("Preview : " , result.preview_url);
     console.log("Album Name : " , result.album.name);
+    console.log("");
+    console.log("===============================================================");
+    console.log("");
 
   })
   .catch(function(error) {
@@ -88,23 +102,28 @@ spotify
 
 };
 
-
 // This will output movie information
 function getMovie(movieName){
 
 //Response if user does not type in a movie title
 if (movieName === undefined) {
   movieName = `Mr.Nobody`;
-  console.log("-----------------------");
+  console.log("");
+  console.log("===================== Watch this =====================");
+  console.log("");
   console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
   console.log("It's on Netflix!");
   
-}
+};
     
 // Run a request with axios to the OMDB API with the movie specified
 axios.get(`https://www.omdbapi.com/?t=${movieName}&y=&plot=short&apikey=trilogy`).then(
 
+
     function(response) {
+      console.log("");
+      console.log("===================== Movie Information =====================");
+      console.log("");
       console.log("Title of the movie : " + response.data.Title);
       console.log("Year the movie came out : " + response.data.Year);
       console.log("IMDB Rating of the movie : " + response.data.imdbRating);
@@ -113,6 +132,9 @@ axios.get(`https://www.omdbapi.com/?t=${movieName}&y=&plot=short&apikey=trilogy`
       console.log("Language of the movie : " + response.data.Language);
       console.log("Plot of the movie : " + response.data.Plot);
       console.log("Actor in the movie : " + response.data.Actors);
+      console.log("");
+      console.log("===============================================================");
+      console.log("");c
     })
     .catch(function(error) {
       
